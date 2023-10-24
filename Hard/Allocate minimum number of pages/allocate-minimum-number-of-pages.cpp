aed237d1c915a54846ac7d36937b96f7c397ef52
Ashutosh Sahoo <128279260+ashu15311@users.noreply.github.com>
2023-10-24 21:45:42 +0530

    Updated Solution - GfG to GitHub

--- Hard/Allocate minimum number of pages/allocate-minimum-number-of-pages.cpp
+++ Hard/Allocate minimum number of pages/allocate-minimum-number-of-pages.cpp
@@ -34,12 +34,12 @@ class Solution
                 }
             }
             
-            if(count<=M){
+            if(count>M)
+                s=mid+1;
+            else{
                 ans=mid;
                 e=mid-1;
             }
-            else
-                s=mid+1;
         }
         return ans;
     }
